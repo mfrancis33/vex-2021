4aeeaa27b94a69792fc11dc84d6dc65f5f6699f8
M. Francis <mfrancis48439@gmail.com>
2021-07-12 20:56:32 -0400

    Set up tank drive
    Set up tank drive for the robot. Also removed some comments auto generated by the competition template.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,17 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// FrontLeftMotor       motor         1               
+// FrontRightMotor      motor         2               
+// BackLeftMotor        motor         3               
+// BackRightMotor       motor         4               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include "cmath"
 
 using namespace vex;
 
@@ -19,70 +27,46 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  // 
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+    // Set motor velocities
+    int leftAxisPercent  = Controller1.Axis3.position(percent);
+    int rightAxisPercent = Controller1.Axis2.position(percent);
+    // Detect if we need to move the motors
+    // The value of 20 is an arbitrary low value to prevent motor waste from
+    // controller drifting. Absolute value is used to ensure that this works
+    // in both directions.
+    if(abs(leftAxisPercent) > 20 || abs(rightAxisPercent) > 20){
+      // Set velocity to the amount the corresponding axis of the controller
+      // is set to.
+      FrontLeftMotor .setVelocity(leftAxisPercent,  percent);
+      BackLeftMotor  .setVelocity(leftAxisPercent,  percent);
+      FrontRightMotor.setVelocity(rightAxisPercent, percent);
+      BackRightMotor .setVelocity(rightAxisPercent, percent);
+    } else {
+      // If no input is detected, set velocities to 0
+      FrontLeftMotor .setVelocity(0, percent);
+      BackLeftMotor  .setVelocity(0, percent);
+      FrontRightMotor.setVelocity(0, percent);
+      BackRightMotor .setVelocity(0, percent);
+    }
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    // DO NOT REMOVE THIS!
+    wait(20, msec);
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
+// DO NOT CHANGE THIS FUNCTION!
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,15 +1,28 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor FrontLeftMotor = motor(PORT1, ratio18_1, false);
+motor FrontRightMotor = motor(PORT2, ratio18_1, false);
+motor BackLeftMotor = motor(PORT3, ratio18_1, false);
+motor BackRightMotor = motor(PORT4, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
