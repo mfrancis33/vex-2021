28bab5c547286a829718644943211ff2eaf1e599
M. Francis <mfrancis48439@gmail.com>
2021-08-02 19:39:43 -0400

    Reversed some motors because I forgot to the first time
    Reversed 3 motors

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -9,11 +9,11 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor FrontLeftMotor = motor(PORT1, ratio18_1, false);
+motor FrontLeftMotor = motor(PORT1, ratio18_1, true);
 motor FrontRightMotor = motor(PORT2, ratio18_1, false);
-motor BackLeftMotor = motor(PORT3, ratio18_1, false);
+motor BackLeftMotor = motor(PORT3, ratio18_1, true);
 motor BackRightMotor = motor(PORT4, ratio18_1, false);
-motor LeftElevator = motor(PORT5, ratio18_1, false);
+motor LeftElevator = motor(PORT5, ratio18_1, true);
 motor RightElevator = motor(PORT6, ratio18_1, false);
 
 // VEXcode generated functions
